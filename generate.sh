01d3c30644e34232f75ad1d6aeb4e5ac35ce7c33
Andrew Beekhof <andrew@beekhof.net>
2022-02-09 13:37:25 +1100

    Fix generation of java sources

--- generate.sh
+++ generate.sh
@@ -30,19 +30,18 @@ if [[ "$BUILDER_IMAGE)" == *"jdk"* ]] || [[ "$BUILDER_IMAGE)" == *"java"* ]]; th
     cat $ARTIFACTS/env-file
     echo "------------------------------"
     podman run -v ./$BUILD_PATH:/sources -v .build:/output quay.io/openshift-pipeline/s2i build --loglevel=$LOGLEVEL /sources $BUILDER_IMAGE --image-scripts-url image:///usr/local/s2i --as-dockerfile /output/Dockerfile.gen --environment-file /output/env-file
-    if [[ -n "$CHAINED_BUILD_DOCKERFILE" ]]; then
-	echo -e "$CHAINED_BUILD_DOCKERFILE" >> $ARTIFACTS/Dockerfile.gen
-    fi
     #buildah bud --tls-verify=$TLSVERIFY --storage-driver=vfs -f $ARTIFACTS/Dockerfile.gen -t $OUTPUT_IMAGE -v $ARTIFACTS/m2:/ba/m2 .
 else
     podman run -v ./$BUILD_PATH:/sources -v .build:/output quay.io/openshift-pipeline/s2i build --loglevel=$LOGLEVEL /sources $BUILDER_IMAGE --as-dockerfile /output/Dockerfile.gen
-    FILES=$(ls -1 $BUILD_PATH | grep -v -e Dockerfile -e '~' -e scripts | tr '\n' ' ')
-    sed -e 's/.*io.openshift.s2i.build.*//g' -e "s@upload/src /tmp/src@$FILES /tmp/src/@" -e "s@upload/scripts@scripts@" -i .build/Dockerfile.gen
-    if [[ -n "$CHAINED_BUILD_DOCKERFILE" ]]; then
-	echo -e "$CHAINED_BUILD_DOCKERFILE" >> .build/Dockerfile.gen
-    fi
     #buildah bud --tls-verify=$TLSVERIFY --storage-driver=vfs -f $ARTIFACTS//Dockerfile.gen -t $OUTPUT_IMAGE .    
 fi
+
+FILES=$(ls -1 $BUILD_PATH | grep -v -e Dockerfile -e '~' -e scripts | tr '\n' ' ')
+sed -e 's/.*io.openshift.s2i.build.*//g' -e "s@upload/src /tmp/src@$FILES /tmp/src/@" -e "s@upload/scripts@scripts@" -i .build/Dockerfile.gen
+if [[ -n "$CHAINED_BUILD_DOCKERFILE" ]]; then
+    echo -e "$CHAINED_BUILD_DOCKERFILE" >> .build/Dockerfile.gen
+fi
+
 cp .build/Dockerfile.gen $BUILD_PATH/Dockerfile
 ls -1 .build/upload/scripts
 cp -r .build/upload/scripts $BUILD_PATH/
